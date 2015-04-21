5376e75eab5f3efdd85bbf0ea29977fc3f8f71a4
Elisa <Elisa@130.190.28.158>
2015-04-21 10:21:28 +0200

    modifications des erreurs de la classe

--- test.java
+++ test.java
@@ -1,21 +1,11 @@
 public class Test{
 	public String t;
 	public int test;
-}
 	private String Tamere;
 	
 	public Test(String t, tam) {
 		t = t;
 		tamere = tam;
-	private String Tamere;
-	public String coucou;
 	
-	public Test(String t, tam) {
-		t = t;
-		tamere = tam;
-		
-		//Comment for the mothers
-		//C'est l'histoire d'une tortue qui voulait courir vite. Elle décida de manger de la viande pur prendre du muscle.....
-		// TEEEEST
 	}
 }
