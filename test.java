353b6e35e4bc579805c18fc82a4a16b0ff1f967e
quent_000 <quent_000@130.190.33.43>
2015-04-21 10:13:50 +0200

    Test Quentin xxx+

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
 public class Test{
 	public String t;
+	public int test;
+}
+	private String Tamere;
+	
+	public Test(String t, tam) {
+		t = t;
+		tamere = tam;
 	private String Tamere;
 	public String coucou;
 	
@@ -9,5 +16,6 @@ public class Test{
 		
 		//Comment for the mothers
 		//C'est l'histoire d'une tortue qui voulait courir vite. Elle décida de manger de la viande pur prendre du muscle.....
+		// TEEEEST
 	}
 }
